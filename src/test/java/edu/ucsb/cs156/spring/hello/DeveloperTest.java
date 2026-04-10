e1194d0809bccb2366dbb8b08a19deeaa6e32b25
Rushabh Taneja <rushabhtanejaaa@gmail.com>
2026-04-10 11:27:59 -0700

    rt - updated with actual github id

--- src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
@@ -24,6 +24,10 @@ public class DeveloperTest {
     public void getName_returns_correct_name() {
         assertEquals("Rushabh", Developer.getName());
     }
+    @Test
+    public void getGithubId_returns_correct_githubId() {
+        assertEquals("RushabhTaneja", Developer.getGithubId());
+    }
 
     // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
     // 100% mutation coverage (all mutants timed out or killed)
