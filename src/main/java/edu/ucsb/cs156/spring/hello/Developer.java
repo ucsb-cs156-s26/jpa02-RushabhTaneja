a4127f9c8db75b62bf52f84836ff135bf3812777
Rushabh Taneja <rushabhtanejaaa@gmail.com>
2026-04-10 15:29:31 -0700

    rt -updates with team names and members

--- src/main/java/edu/ucsb/cs156/spring/hello/Developer.java
+++ src/main/java/edu/ucsb/cs156/spring/hello/Developer.java
@@ -26,7 +26,6 @@ public class Developer {
      */
 
     public static String getGithubId() {
-        // TODO: Change this to your github id
         return "RushabhTaneja";
     }
 
@@ -36,14 +35,13 @@ public class Developer {
      */
     
     public static Team getTeam() {
-        // TODO: Change this to your team name
-        Team team = new Team("s26-xx");
-        team.addMember("Alice");
-        team.addMember("Bob");
-        team.addMember("Chris G.");
-        team.addMember("Danny");
-        team.addMember("Eve");
-        team.addMember("Frances");
+        Team team = new Team("s26-11");
+        team.addMember("Anish");
+        team.addMember("Brian H.");
+        team.addMember("Nini");
+        team.addMember("Rushabh");
+        team.addMember("Tien");
+        team.addMember("Wilson");
         return team;
     }
 }
